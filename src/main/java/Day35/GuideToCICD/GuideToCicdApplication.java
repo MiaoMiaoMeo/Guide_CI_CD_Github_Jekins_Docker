6af86b37067de145414c6d414c35dedd289ef8ef
MiaoMiaoMeo <47258086+MiaoMiaoMeo@users.noreply.github.com>
2023-04-09 12:55:14 -0400

    Initial commit

---
+++ src/main/java/Day35/GuideToCICD/GuideToCicdApplication.java
@@ -0,0 +1,13 @@
+package Day35.GuideToCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideToCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideToCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Day35/GuideToCICD/GuideToCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package Day35.GuideToCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideToCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
